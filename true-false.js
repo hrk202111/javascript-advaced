052f59740aeb456df6b62b30d0ba90a4abbffc44
hrk202111 <hrk202111@gmail.com>
2020-08-22 00:53:05 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// False value
+// 0
+// ""
+// undefined  = value set na korle
+// null
+// NaN
+//false
+
+// Truthy value
+// " "
+// "0"
+// []
+// {}
+
+
+
+
+let name = 12;
+if(name || name ==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
