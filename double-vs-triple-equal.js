28781c5fa9dc6e9b7adcb38c11f3060e675a931f
hrk202111 <hrk202111@gmail.com>
2020-08-22 01:43:45 +0600

    done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,13 @@
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+// == ata diye shudhu value check kore
+// === ata diye value and type dutoi check kore
